ccabbf67302ccdde5d756ffcf720c5bb76b25a80
damengrandom <damonmaozewu@gmail.com>
2025-07-04 21:47:41 +1000

    added interview firebase api endpoint to post questions to firebase to store it

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAvSN5SAMYHHWVDfzaHg1WTqXw5V-1MkxA",
-  authDomain: "aiinterviewer-85cf3.firebaseapp.com",
-  projectId: "aiinterviewer-85cf3",
-  storageBucket: "aiinterviewer-85cf3.firebasestorage.app",
-  messagingSenderId: "1018003159051",
-  appId: "1:1018003159051:web:97907ed78fa537fcc02f0e",
-  measurementId: "G-RE1VP0GPVW"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
